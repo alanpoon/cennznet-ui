2326c0b6a0ee48305849c52a4a42fe2ce3caa160
alanpoon <brightshadowx@gmail.com>
2019-07-10 22:47:11 +0800

    t

--- packages/app-xpay/src/use.tsx
+++
@@ -1,5 +0,0 @@
-const execute = require("./public/execute");
-// declare function execute():any;
-export default function(image:any,gentleman:any,lady:any){
-    execute.default(image,gentleman,lady);
-}
